fd60b1cdae39d6512d03418760ebbdd874d2b96d
Joseph Martinot-Lagarde <joseph.martinot-lagarde@onera.fr>
2022-02-06 13:49:14 +0100

    Include test scripts

---
+++ example/profiling_test_script.py
@@ -0,0 +1,54 @@
+import time
+import os
+from pprint import pprint
+
+import subdir.profiling_test_script2 as script2
+
+
+@profile
+def fact(n):
+    result = 1
+    for i in range(2, n // 4):
+        result *= i
+    result = 1
+    # This is a comment
+    for i in range(2, n // 16):
+        result *= i
+    result = 1
+
+    if False:
+        # This won't be run
+        raise RuntimeError("What are you doing here ???")
+
+    for i in range(2, n + 1):
+        result *= i
+    return result
+    # This is after the end of the function.
+
+    if False:
+        # This won't be run
+        raise RuntimeError("It's getting bad.")
+
+
+@profile
+def wont_run(n):
+    # Don't try this at home!
+    while True:
+        time.sleep(1)
+
+
+@profile
+def sum_(n):
+    result = 0
+
+    for i in range(1, n + 1):
+        result += i
+    return result
+
+
+if __name__ == "__main__":
+    print(fact(120))
+    print(sum_(120))
+    print(script2.fact2(120))
+    print(script2.sum2(120))
+    pprint(dict(os.environ))
---
+++ example/subdir/profiling_test_script2.py
@@ -0,0 +1,18 @@
+@profile
+def fact2(n):
+    result = 1
+    for i in range(2, n + 1):
+        result *= i * 2
+    return result
+
+
+def sum2(n):
+    result = 0
+    for i in range(1, n + 1):
+        result += i * 2
+    return result
+
+
+if __name__ == "__main__":
+    print(fact2(120))
+    print(sum2(120))

b89b96b77ca845e1f2016f483bd8c1b3838beca3
Joseph Martinot-Lagarde <contrebasse@gmail.com>
2023-09-10 22:09:40 +0200

    Use explicit os.chdir

--- tests/utils.py
+++ tests/utils.py
@@ -1,14 +1,19 @@
+import os
 import textwrap
+from pathlib import Path
 
 from lineprofilergui import main
 
 
-def run_code(code, tmp_path, qtbot):
+def run_code(code: str, tmp_path: Path, qtbot):
     """Define helper function to run profiled code from UIMainWindow."""
     scriptfile = tmp_path / "script.py"
     scriptfile.write_text(textwrap.dedent(code))
 
-    with tmp_path:
+    current_path = Path.cwd()
+    try:
+        os.chdir(tmp_path)
+
         main.icons_factory()
         win = main.UIMainWindow()
         qtbot.addWidget(win)
@@ -17,5 +22,7 @@ def run_code(code, tmp_path, qtbot):
 
         with qtbot.waitSignal(win.profile_finished, timeout=10000):
             win.actionRun.trigger()
+    finally:
+        os.chdir(current_path)
 
     return win
